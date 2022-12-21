f7f0b071440a6bff420f411a62db22da037ebf7b
wisutpeth <s6406021610038@email.kmutnb.ac.th>
2022-12-21 10:52:42 +0700

    update

---
+++ lec2/nummath.js
@@ -0,0 +1,19 @@
+let num1 = 100;
+
+//Baic Math
+console.log("********Basic Math********");
+console.log(num1 + 25);
+console.log(num1 - 100);
+console.log(num1 * 100);
+console.log(num1 / 1500);
+
+console.log("********Additional Mathematical Operations********");
+//Additional Mathmatical Operations
+console.log(num1 % 1500); // Reminder
+console.log(++num1); //Increment
+console.log(--num1); //Decrement
+
+console.log("********Using the Math Object********");
+//Using the math object
+console.log(Math.PI); //Pi
+console.log(Math.sqrt(num1)); //Square root
